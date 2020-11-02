90ceb8a1e19f94ac7445ceffbabc46dfbc3314db
salis <salisshuaibu11@gmail.com>
2020-11-02 12:55:03 +0000

    functionality for next, prev, play and progress

--- js/index.js
+++ js/index.js
@@ -27,8 +27,8 @@ function loadSong(song) {
 // Play song
 function playSong() {
   musicContainer.classList.add('play');
-  playBtn.querySelector('i.fas').classList.remove('fa-play');
-  playBtn.querySelector('i.fas').classList.add('fa-pause');
+  playBtn.querySelector('svg').classList.remove('fa-play');
+  playBtn.querySelector('svg').classList.add('fa-pause');
 
   audio.play();
 }
@@ -36,10 +36,52 @@ function playSong() {
 // Pause song
 function pauseSong() {
   musicContainer.classList.remove('play');
-  playBtn.querySelector('i.fas').classList.add('fa-play');
-  playBtn.querySelector('i.fas').classList.remove('fa-pause');
+  playBtn.querySelector('svg').classList.add('fa-play');
+  playBtn.querySelector('svg').classList.remove('fa-pause');
 
   audio.pause();
+}
+
+// Previous song
+function prevSong() {
+  songIndex--;
+
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+
+  loadSong(songs[songIndex]);
+
+  playSong();
+};
+
+// Next song
+function nextSong() {
+  songIndex++;
+
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  };
+
+  loadSong(songs[songIndex]);
+
+  playSong();
+};
+
+// Update progress bar
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+};
+
+// Set progress bar
+function setProgress(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const duration = audio.duration;
+
+  audio.currentTime = (clickX / width) * duration;
 }
 
 // Event listeners
@@ -51,4 +93,17 @@ playBtn.addEventListener('click', () => {
   } else {
     playSong();
   }
-})
+});
+
+// Change song
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+
+// Time/song update
+audio.addEventListener('timeupdate', updateProgress);
+
+// Click on progress bar
+progressContainer.addEventListener('click', setProgress);
+
+// SOng ends
+audio.addEventListener('ended', nextSong);
